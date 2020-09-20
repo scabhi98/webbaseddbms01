f6abe54416be36b276a2b689bf3355387721b980
Kunal Sen <rockstarkunal17@hotmail.com>
2020-09-20 21:51:37 +0530

    Sign Up Page added

--- scripts/theme.switch.js
+++ scripts/theme.switch.js
@@ -2,7 +2,7 @@
 var lightTheme = document.createElement('link');
 var themeSwitchBtn = document.getElementById('themeSwitchBtn');
 var head  = document.getElementsByTagName('head')[0];
-lightTheme.href = "styles/cutestrap/cutestrap.light.theme.css";
+lightTheme.href = "/styles/cutestrap/cutestrap.light.theme.css";
 lightTheme.rel = 'stylesheet';
 lightTheme.type = 'text/css';
 lightTheme.media = 'all';
