002e093cb2d63411bb3f7c989d46a75dfa5ecdd1
KUNAL SEN <rockstarkunal17@hotmail.com>
2020-10-16 20:39:23 +0530

    merge confilct

--- scripts/custom.functions.js
+++ scripts/custom.functions.js
@@ -49,69 +49,6 @@ document.write( generateOTP() );
 */
 
 
-<<<<<<< HEAD
-var myInput = document.getElementById("password");
-var letter = document.getElementById("letter");
-var capital = document.getElementById("capital");
-var number = document.getElementById("number");
-var length = document.getElementById("length");
-
-// When the user clicks on the password field, show the message box
-myInput.onfocus = function() {
-  document.getElementById("message1").style.display = "block";
-}
-
-// When the user clicks outside of the password field, hide the message box
-myInput.onblur = function() {
-  document.getElementById("message1").style.display = "none";
-}
-
-// When the user starts to type something inside the password field
-myInput.onkeyup = function() {
-  // Validate lowercase letters
-  var lowerCaseLetters = /[a-z]/g;
-  if(myInput.value.match(lowerCaseLetters)) {  
-    letter.classList.remove("invalid");
-    letter.classList.add("valid");
-  } else {
-    letter.classList.remove("valid");
-    letter.classList.add("invalid");
-  }
-  
-  // Validate capital letters
-  var upperCaseLetters = /[A-Z]/g;
-  if(myInput.value.match(upperCaseLetters)) {  
-    capital.classList.remove("invalid");
-    capital.classList.add("valid");
-  } else {
-    capital.classList.remove("valid");
-    capital.classList.add("invalid");
-  }
-
-  // Validate numbers
-  var numbers = /[0-9]/g;
-  if(myInput.value.match(numbers)) {  
-    number.classList.remove("invalid");
-    number.classList.add("valid");
-  } else {
-    number.classList.remove("valid");
-    number.classList.add("invalid");
-  }
-  
-  // Validate length
-  if(myInput.value.length >= 8) {
-    length.classList.remove("invalid");
-    length.classList.add("valid");
-  } else {
-    length.classList.remove("valid");
-    length.classList.add("invalid");
-  }
-}
-
-
-
-=======
->>>>>>> 21b3e85f4be851925e7efb35ef80bd297b79c07d
 //clic dropdown script
 
 /* When the user clicks on the button, 
@@ -140,4 +77,4 @@ let dropdownBtn = document.querySelector('.cs-circular-btn');
 let menuContent = document.querySelector('.cs-dropdown-content');
 dropdownBtn.addEventListener('click',()=>{
    menuContent.classList.toggle('cs-hide');
-})
+});
--- scripts/trikon.js
+++ scripts/trikon.js
@@ -271,11 +271,6 @@ var route = window.route = function(url,target){
     }
     else{
         trikon.ajax({url: comp.templateUrl}).then(data => {
-<<<<<<< HEAD
-            trikon.rootComponent.outlet.innerHTML = comp.template = data;
-            if(comp.afterViewParsed)
-                comp.afterViewParsed(trikon.rootComponent.outlet);
-=======
             comp.template = data;
             var view = trikon.domParser.parseFromString(data, 'text/html').body;
             if(comp.onViewCreated)
@@ -283,7 +278,6 @@ var route = window.route = function(url,target){
             trikon.rootComponent.outlet.innerHTML = '';
             trikon.rootComponent.outlet.appendChild(view);
             
->>>>>>> 21b3e85f4be851925e7efb35ef80bd297b79c07d
         });
     }
     
